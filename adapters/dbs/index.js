800c4e3a2283a8ca4b03709dbb6865f36b0ad9a3
janbialostok <janbialostok@gmail.com>
2016-11-28 15:49:50 -0500

    added jsdoc for interface

--- adapters/dbs/index.js
+++ adapters/dbs/index.js
@@ -5,12 +5,25 @@ const ADPATERS = {
 	mongo: require(path.join(__dirname, './mongo'))
 };
 
+/**
+ * Interface class - defines properties and property types that should exist within constructed classes
+ */
 const DB_ADAPTER_INTERFACE = class DB_ADAPTER_INTERFACE {
+	/**
+	 * Creates an interface
+	 * @param  {Object} [options={}] A set of properties defined by keys with their allowed types as values. Each property will be required by newly constructed classes from this interface
+	 */
 	constructor (options = {}) {
 		for (let key in options) {
 			this[key] = options[key];
 		}
 	}
+	/**
+	 * Constructs a new object with a prototype defined by the .adapter ensuring that instantiated class conforms to interface requirements
+	 * @param  {Object} [options={}] Values to be passed to class constructor (.adapter should be reserved for either customer class or string that matches key in ADAPTERS)
+	 * @param {string|Function} options.adapter Required to specify type of adapter to be constructed or a class constructor that can be instantiated with new keyword
+	 * @return {Object}         Returns an instantiated adapter class
+	 */
 	create (options = {}) {
 		let Adapter = (typeof options.adapter === 'string') ? ADPATERS[options.adapter] : options.adapter;
 		if (!Adapter) throw new Error('Could not find a corresponding adapter - for custom adapters pass the constructor as the "adapater" options');
