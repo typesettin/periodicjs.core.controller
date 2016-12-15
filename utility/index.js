1fd06fc996a6c3c90798c55d09577c00cc949928
janbialostok <janbialostok@gmail.com>
2016-12-15 17:50:18 -0500

    moved aliased function files

--- utility/index.js
+++ utility/index.js
@@ -1,9 +1,9 @@
 'use strict';
 const path = require('path');
 const traps = require(path.join(__dirname, './traps'));
-const loggers = require(path.join(__dirname, './aliased_logger'));
+const loggers = require(path.join(__dirname, './aliased/aliased_logger'));
 const middleware = require(path.join(__dirname, './middleware'));
-const protocols = require(path.join(__dirname, './aliased_protocol'));
+const protocols = require(path.join(__dirname, './aliased/aliased_protocol'));
 const aliased = Object.assign({}, loggers, protocols);
 
 module.exports = { traps, aliased, middleware };
